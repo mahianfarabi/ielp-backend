96f4431b3a3a90baf3a24c108b47f26fa797a435
Mahian Farabi <mahianfarabi@gmail.com>
2025-11-10 12:03:59 +0600

    Adding backend to git

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error(err));
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
