1e94bcd9e17076e17c9e97155f3afb236b7053d7
Mahian Farabi <mahianfarabi@gmail.com>
2025-11-10 13:53:08 +0600

    backens server fixation

--- server.js
+++ server.js
@@ -10,9 +10,31 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error(err));
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch(err => console.error(err));
+
+let isConnected = false;
+async function connectToDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
+
+// add middleware to check and connect to DB
+app.use(async (req, res, next) => {
+  if (!isConnected) {
+    await connectToDB();
+  }
+  next();
+});
 
 // Sample route
 app.get("/", (req, res) => {
@@ -20,5 +42,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
+
